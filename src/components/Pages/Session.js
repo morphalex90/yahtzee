6a1270d54e6f25c7894b4615026111047b9184dd
morphalex90 <piero.nanni@gmail.com>
2020-07-27 02:24:49 +0100

    add buttons for leave and confirm

--- src/components/Pages/Session.js
+++ src/components/Pages/Session.js
@@ -5,10 +5,7 @@ import Footer from './../Layout/Footer';
 
 import axios from 'axios';
 
-// import { NavLink } from 'react-router-dom';
-
 import Pusher from 'pusher-js';
-
 
 import API_URL from './../API_URL';
 
@@ -33,15 +30,20 @@ class Session extends React.Component {
       try {
         var pusher = new Pusher('1717a821a4ce3aea5ba0', { cluster: 'eu' });
         var channel = pusher.subscribe('players');
-        channel.bind('player-join-session', data => {
-          this.setState({ players: [...this.state.players, data.message], playersCount: this.state.playersCount+1 });
+        channel.bind('player-join-session', data => { // player join session
+          this.setState({ players: [...this.state.players, data.message], playersCount: this.state.playersCount+1 }); // add new player to the list and update the players counter
+        });
+
+        channel.bind('player-leave-session', data => { // player leave session
+          // this.setState({ players: [...this.state.players, data.message], playersCount: this.state.playersCount-1 }); // leave player from list and update the players counter
+          console.log(data.message);
         });
       } catch (error) {
         console.error(error);
       }
     }
   
-    async getPlayers() {
+    async getPlayers() { // get list of players on page load
     axios
       .get(API_URL+'/api/v1/yahtzee/session/'+this.props.match.params.session+'/players')
       .then((response) => {
@@ -57,7 +59,7 @@ class Session extends React.Component {
       this.setState({ [e.target.name]: e.target.value });
     }
   
-    playerSubmit = (e) => {
+    playerSubmit = (e) => { // a new player wants to join this session
       this.setState({ isLoading: true });
       e.preventDefault();
       let data = new FormData(e.target);
@@ -66,6 +68,31 @@ class Session extends React.Component {
       .then((response) => {
         if( response.status === 200 ) {
           this.setState({ message_status: 'success', message_text: 'You are now added to the game', playername: '' ,hide_join_form: true }); //, () => this.getPlayers()
+        }
+        this.setState({ isLoading: false });
+      })
+      .catch((error) => {
+        if (error.response && error.response.status === 422) { 
+          this.setState({ message_status: 'warning', message_text: error.response.data.playername[0] });
+        } else {
+          console.log(error);
+          this.setState({ message_status: 'error', message_text: 'Something went bananas, contact the administrator' });
+        }
+        this.setState({ isLoading: false });
+      });
+    }
+
+    playerLeave = (e) => { // a player wants to leave this session
+      this.setState({ isLoading: true });
+      e.preventDefault();
+      let player_id = e.target.player_id.value;
+      let session_id = e.target.session_id.value;
+
+      axios
+      .post(API_URL+'/api/v1/yahtzee/session/'+session_id+'/player/'+player_id+'/leave')
+      .then((response) => {
+        if( response.status === 200 ) {
+          this.setState({ message_status: 'success', message_text: 'You now left the session', hide_join_form: false });
         }
         this.setState({ isLoading: false });
       })
@@ -96,6 +123,19 @@ class Session extends React.Component {
                       <div key={player.id} className="session-players__player" data-status={player.status}>
                         <div className="session-players__player__name">{player.playername}</div>
                         <div className="session-players__player__points"> ,points: {player.points}</div>
+
+                        <form className="session-players__leave" onSubmit={this.playerLeave}>
+                          <input type="hidden" name="player_id" value={player.id} />
+                          <input type="hidden" name="session_id" value={this.props.match.params.session} />
+                          <button type="submit">Leave session</button>
+                        </form>
+
+                        <form className="session-players__ready" onSubmit={this.playerReady}>
+                          <input type="hidden" name="player_id" value={player.id} />
+                          <input type="hidden" name="session_id" value={this.props.match.params.session} />
+                          <button type="submit">Ready to play!</button>
+                        </form>
+
                       </div>
                     )}
                   </div>
