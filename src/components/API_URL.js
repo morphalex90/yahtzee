5ff0ffe51d4cb7a8815578e6b58e44f33b57c365
morphalex90 <piero.nanni@gmail.com>
2022-03-19 22:01:31 +0000

    change api url

--- src/components/API_URL.js
+++ src/components/API_URL.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://phplaravel-382225-1341568.cloudwaysapps.com';
+export const API_URL = 'https://phplaravel-746788-2513350.cloudwaysapps.com';
 // export const API_URL = 'http://api.local';
 
 export default API_URL;
